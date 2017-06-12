c11acc385eee9ed78d9f64a2333c7dfe9250c0fa
Brittany Starr <lunarunar@gmail.com>
2017-06-11 17:30:39 -0700

    all functionality completed

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -1,30 +1,36 @@
-/*
-  The user can resubmit a new list that would override the previous one
-
-  Display the household list in the HTML as it is modified
-
-  Serialize the household as JSON upon form submission as a fake trip to the server
-    On submission, put the serialized JSON in the provided "debug" DOM element and display that element.
-
-  After submission the user should be able to make changes and submit the household again.
-*/
-
-window.onload = function() {
-  var list = document.createElement('ul');
-  list.className = 'addedMembers';
-  document.body.appendChild(list);
-};
-
 var familyData = {};
 var mbID = 0;
 
-document.getElementsByClassName('add')[0].addEventListener('click', function() {
+window.onload = createMemberList();
+
+document.getElementsByClassName('add')[0].addEventListener('click', function(e) {
+  e.preventDefault();
+  addMember();
+});
+
+document.getElementsByTagName('button')[1].addEventListener('click', function(e) {
+  e.preventDefault();
+  submitForm();
+});
+
+function createMemberList() {
+  var list = document.createElement('ul');
+  list.className = 'addedMembers';
+  list.style.display = 'inline-block';
+  list.style.float = 'left';
+  list.style.width = '50%';
+
+  document.body.append(list);
+}
+
+function addMember() {
   var ageValue = document.getElementsByName('age')[0].value;
   var relValue = document.getElementsByName('rel')[0].value;
   var smokerValue = document.getElementsByName('smoker')[0].checked;
   var smokerStatus = 'Non-Smoker';
   var member = document.createElement('li');
   var data = ageValue + ', ' + relValue + ', ' + smokerStatus;
+  var memberList;
 
   if (!ageValue.length && !relValue.length) {
     alert('Please complete the form with valid data. If you\'re done adding your family members, please click submit instead.');
@@ -34,28 +40,30 @@ document.getElementsByClassName('add')[0].addEventListener('click', function() {
     smokerStatus = 'Smoker';
   }
 
-  familyData[mbID] = data;
+  familyData[mbID] = {
+    age: ageValue,
+    relationship: relValue,
+    smoker: smokerValue
+  };
 
   member.className = 'member';
   member.id = mbID;
   member.innerHTML = ageValue + ', ' + relValue + ' (' + smokerStatus + ') - <span class="remove" id="' + mbID + '"style="font-weight: bold; text-decoration: underline;">remove member</span>';
 
-  document.body.appendChild(member);
+  document.getElementsByTagName('ul')[0].appendChild(member);
+  attachListeners();
   mbID++;
-});
+}
 
-document.body.addEventListener('click', function(e) {
-  e.preventDefault();
-  var clickedID;
-  if (e.target.className === 'remove') {
-    clickedID = e.target.id;
-    removeMb(clickedID);
-  }
-
-  if (e.target.textContent === 'submit') {
-    updateJSON();
-  }
-});
+function attachListeners() {
+  memberList = Array.from(document.getElementsByClassName('remove'));
+  memberList.forEach(function(item) {
+    item.addEventListener('click', function(e) {
+      var clickedID = e.target.id;
+      removeMb(clickedID);
+    });
+  });
+}
 
 function removeMb(clickedID) {
   var allMbs = Array.from(document.getElementsByClassName('member'));
@@ -71,9 +79,13 @@ function removeMb(clickedID) {
   }
 };
 
-function updateJSON() {
+function submitForm() {
   var stringified = JSON.stringify(familyData, undefined, 2);
   var debug = document.getElementsByClassName('debug')[0];
+
+  if (stringified.length <= 2) {
+    debug.style.display = 'none';
+  }
 
   if (debug.childNodes.length) {
     debug.removeChild(debug.firstChild);
@@ -82,4 +94,5 @@ function updateJSON() {
     debug.append(stringified);
     debug.style.display = 'inline-block';
   }
+
 };
